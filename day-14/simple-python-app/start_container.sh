b7868731f36f1d6e68669ef07af8e92b333e3689
HuaBeiPingYuan <yaohua13liluye@gmail.com>
2025-11-21 13:15:47 +0200

    add S3 permissions

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-containerID=docker ps | awk -F" " '{Print $1}'
-
-docker rm -f $containerID
-wget https://aws-codedeploy-eu-north-1.s3.eu-north-1.amazonaws.com/latest/install
+# Stop the running container (if any)
+echo "Hi"
--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+containerID=docker ps | awk -F" " '{Print $1}'
+
+docker rm -f $containerID
+wget https://aws-codedeploy-eu-north-1.s3.eu-north-1.amazonaws.com/latest/install
